45ea50d362ede283ea99281f5f25108dcf6a6f21
Scott-Salerno74 <sws001@lvc.edu>
2019-02-15 09:47:24 -0500

    Added Puzzle Class

---
+++ src/Puzzle.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.util.Random;
+
+public class Puzzle {
+    // size of the puzzle
+    private int size;
+    // number of tiles in puzzle
+    private int numTiles;
+    // Dimensions of UI
+    private int dimensions;
+    //Foreground Color
+    private static final Color foreGround = Color.getColor();
+
+
+}
