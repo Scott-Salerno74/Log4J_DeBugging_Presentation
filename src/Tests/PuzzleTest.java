42b09c91424536b85d38122d76af32c3314e4667
Scott-Salerno74 <sws001@lvc.edu>
2019-02-18 15:42:47 -0500

    Added Some changes and created Tests and Logs Directory, Created on Test for Puzzle Class to show what I am doing

---
+++ src/Tests/PuzzleTest.java
@@ -0,0 +1,14 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+public class PuzzleTest {
+
+    @org.junit.Test
+    public void paintComponent() {
+    }
+
+    @org.junit.Test
+    public void run() {
+    }
+}
